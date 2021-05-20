0f06c692804feb9a31dd6b06edea3a40784d0b6b
荣顶 <wangrongding@qq.com>
2021-05-20 13:54:12 +0800

    feat: Node环境下的一款小巧可爱的翻译工具

---
+++ README.md
@@ -0,0 +1,19 @@
+# ding-trans (ding 翻译)
+
+## 安装
+
+```
+npm i ding-trans -g
+```
+
+### 使用>
+
+```
+ding hello
+```
+
+![1621489715(1).jpg](http://ww1.sinaimg.cn/large/75314ac9ly1gqouh4l13sj20g6073749.jpg)
+
+### 支持我
+
+See [荣顶](https://www.fedtop.com).
---
+++ bin/translate.js
@@ -0,0 +1,101 @@
+#! /usr/bin/env node
+
+//业务逻辑
+//1.turnout命令+中英文[args,--read,-r]
+//捕获参数，使用yargs.argv对象；
+//2.将捕获到的用户的输入参数传入有道云api的q（key）中，使用字符串拼接；
+//3.使用http模块发送请求；
+//4.成功接收数据后，对数据进行格式化处理；
+
+let colors = require("colors");
+let argv = require("yargs").argv,
+    queryStr = encodeURI(argv._.join(" ")),
+    read = argv.r || argv.read;
+
+if (!queryStr) {
+    console.log("word or sentence required...");
+} else {
+    sendInfo(queryStr);
+}
+
+if (read) {
+    console.log("waiting for the new function...");
+}
+
+function format(json) {
+    // console.log("\n");
+    // console.log(JSON.parse(json), "\n");
+
+    let data = JSON.parse(json),
+        pronTitle = "发音：",
+        pron = data.basic ? data.basic.phonetic : "无",
+        mainTitle = "翻译：",
+        mainTrans = "",
+        webTitle = "网络释义：",
+        webTrans = "",
+        template = "";
+
+    let basic = data.basic,
+        web = data.web;
+
+    if (basic ? basic : "") {
+        for (let i = 0; i < basic.explains.length; i++) {
+            mainTrans += "\n" + basic.explains[i];
+        }
+    }
+
+    if (web ? web : "") {
+        for (let i = 0; i < web.length; i++) {
+            webTrans +=
+                "\n" +
+                (i + 1) +
+                ": " +
+                web[i].key.red.bold +
+                "\n" +
+                web[i].value.join(",");
+        }
+    }
+
+    template =
+        pronTitle.red.bold +
+        pron +
+        "\n" +
+        mainTitle.green.bold +
+        mainTrans +
+        "\n" +
+        webTitle.blue.bold +
+        webTrans;
+
+    console.log(template);
+}
+
+function sendInfo(query) {
+    //发送翻译请求
+    let http = require("http");
+
+    // 1.用于请求的选项
+    let options = {
+        host: "fanyi.youdao.com",
+        port: "80",
+        path:
+            "/openapi.do?keyfrom=translation-tool&key=1730699468&type=data&doctype=json&version=1.1&q=" +
+            query,
+    };
+
+    // 处理响应的回调函数
+    let callback = function (response) {
+        // 不断更新数据
+        response.on("data", function (data) {
+            format(data);
+        });
+
+        response.on("end", function () {
+            // 数据接收完成
+            console.log("\n");
+            console.log("---------------");
+        });
+    };
+    // 向服务端发送请求
+    let req = http.request(options, callback);
+    req.end();
+}
